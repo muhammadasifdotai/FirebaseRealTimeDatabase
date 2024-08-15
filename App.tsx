c97907832ed3a80cf264a48cecb871f531ad7f3c
muhammadasifdotai <sethrolin2573@gmail.com>
2024-08-15 07:57:56 +0500

    Firebase real done

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+import database from '@react-native-firebase/database';
 
-export default function App(): JSX.Element {
+export default function App() {
+  const [myData, setMyData] = useState(null);
+
+  useEffect(() => {
+    getDatabase();
+  }, []);
+
+  const getDatabase = async () => {
+    try {
+      const data = await database().ref('users/1').once('value');
+
+      console.log(data);
+
+      setMyData(data.val());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <View style={styles.main}>
-      <Text style={styles.text}>Alhamdulillah</Text>
+    <View>
+      <Text>Name:-{myData ? myData.name : 'Loading...'}</Text>
+      <Text>Age:-{myData ? myData.age : 'Loading...'}</Text>
     </View>
-  )
+  );
 }
-
-const styles = StyleSheet.create({
-  main: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-  },
-  text: {
-    fontSize: 30,
-    color: 'green',
-  }
-})
